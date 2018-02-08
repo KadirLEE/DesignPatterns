5485ee65e18b36a58b1592cd8a49af57d7057cd2
KadirLEE <36199750+KadirLEE@users.noreply.github.com>
2018-02-08 23:36:44 +0400

    Add files via upload

---
+++ ComputerInfo/ComputerInfo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ComputerInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReportControl reportControl = new ReportControl();
+            reportControl.GetInfo = new GetGPUInfo();
+            reportControl.ComponentReport();
+
+            reportControl.GetInfo = new GetCPUInfo();
+            reportControl.ComponentReport();
+
+            reportControl.GetInfo = new GetHDDInfo();
+            reportControl.ComponentReport();
+        }
+
+        interface IGetInfo
+        {
+            void GetInfo();
+        }
+
+        class GetGPUInfo : IGetInfo
+        {
+            public void GetInfo()
+            {
+                Console.WriteLine("GPU: NVIDIA GeForce 1080i\n");
+            }
+        }
+
+        class GetCPUInfo : IGetInfo
+        {
+            public void GetInfo()
+            {
+                Console.WriteLine("CPU: Intel CORE i9-7980xe\n");
+            }
+        }
+
+        class GetHDDInfo : IGetInfo
+        {
+            public void GetInfo()
+            {
+                Console.WriteLine("HDD: Toshiba 6TB");
+            }
+        }
+
+        class ReportControl
+        {
+            private IGetInfo getInfo;
+
+            public IGetInfo GetInfo
+            {
+                get { return getInfo; }
+                set { getInfo = value; }
+            }
+
+            public void ComponentReport()
+            {
+                getInfo.GetInfo();
+            }
+        }
+    }
+}
